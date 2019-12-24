eab3e6e53fbca17f864725ccef9886367a03b87c
Blackisher <36049543+blackisher@users.noreply.github.com>
2019-12-24 23:57:27 +0100

    LAB11Task1 this should not go to task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
 import rootReducer from "../redux/reducers";
 
 import PageEmployeesList from "./PageEmployeesList";
 import PageEmployeeCreate from "./PageEmployeeCreate";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer, {}, composeWithDevTools());
 
 const App = () => (
   <Provider store={store}>
